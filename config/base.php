8cdefb9c5403d75cd414d4392918947c56e4dba1
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-08-05 17:10:28 +0200

    Server removed

--- config/base.php
+++ config/base.php
@@ -1,13 +1,5 @@
 <?php
 
 return array(
-	'server' => array(
-		'php'     => 'php',
-		'port'    => '8000',
-		'host'    => 'localhost',
-		'docroot' => 'public' . DS,
-		'router'  => null,
-		'pid'     => APPPATH . 'tmp' . DS . 'server.pid'
 
-	)
 );
