0a918dc460085b81bf7fa08d695e4191a78b30ba
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-11-14 02:55:20 +0100

    Migration redesign, enum migrations added

---
+++ migrations/003_create_enums.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_enums
+{
+	public function up()
+	{
+		\DBUtil::create_table('enums', array(
+			'id'          => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'name'        => array('constraint' => 64, 'type' => 'varchar'),
+			'slug'        => array('constraint' => 67, 'type' => 'varchar'),
+			'description' => array('type' => 'text', 'null' => true),
+			'default'     => array('type' => 'int', 'default' => 1, 'unsigned' => true),
+			'active'      => array('type' => 'tinyint', 'default' => 1),
+			'read_only'   => array('type' => 'tinyint', 'default' => 0),
+		), array('id'));
+
+		\DBUtil::create_index('enums', 'slug', 'index_enums_on_slug', 'UNIQUE');
+
+		\DBUtil::create_table('enum_items', array(
+			'id'          => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'enum_id'     => array('constraint' => 11, 'type' => 'int', 'unsigned' => true),
+			'item_id'     => array('constraint' => 11, 'type' => 'int', 'unsigned' => true),
+			'name'        => array('constraint' => 64, 'type' => 'varchar'),
+			'slug'        => array('constraint' => 67, 'type' => 'varchar'),
+			'description' => array('type' => 'text', 'null' => true),
+			'active'      => array('type' => 'tinyint', 'default' => 1),
+			'sort'        => array('constraint' => 11, 'type' => 'int', 'null' => true),
+		), array('id'));
+
+		\DBUtil::create_index('enum_items', 'slug', 'index_enums_on_slug', 'UNIQUE');
+		\DBUtil::create_index('enum_items', 'item_id', 'index_enum_items_on_item_id', 'INDEX');
+		\DBUtil::create_index('enum_items', array('enum_id', 'item_id'), 'index_enum_items_on_enum_id_item_id', 'UNIQUE');
+
+		\DBUtil::add_foreign_key('enum_items', array(
+			'constraint' => 'fk_index_enum_items_on_enum_id',
+			'key' => 'enum_id',
+			'reference' => array(
+				'table' => 'enums',
+				'column' => 'id',
+			),
+			'on_update' => 'CASCADE',
+			'on_delete' => 'CASCADE'
+		));
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_table('enum_items');
+		\DBUtil::drop_table('enums');
+	}
+}
