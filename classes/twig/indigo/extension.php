0adacead56ba50d1bd3663501693863cc3b4db49
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-11-28 18:51:06 +0100

    Twig Extension added things

--- classes/twig/indigo/extension.php
+++ classes/twig/indigo/extension.php
@@ -21,6 +21,8 @@ class Twig_Indigo_Extension extends Twig_Extension
 			'menu'            => new Twig_Function_Function('Menu::render_menu'),
 			'admin_menu'      => new Twig_Function_Function('Menu_Admin::render_menu'),
 			'request'         => new Twig_Function_Function('Request::active'),
+			'admin_url'       => new Twig_Function_Function('Uri::admin'),
+			'current_url'     => new Twig_Function_Function('Uri::current'),
 			'default_img'     => new Twig_Function_Method($this, 'getDefaultImage'),
 		);
 	}
@@ -29,7 +31,19 @@ class Twig_Indigo_Extension extends Twig_Extension
 	{
 
 		return array(
-			'md5'      => new Twig_Filter_Function('md5'),
+			'md5'       => new Twig_Filter_Function('md5'),
+			'pluralize' => new Twig_Filter_Function('Inflector::pluralize'),
+			'bytes'     => new Twig_Filter_Function('Num::format_bytes'),
+			'qty'       => new Twig_Filter_Function('Num::quantity'),
+			'bool'      => new Twig_Filter_Function([$this, 'bool']),
+			'attr'      => new Twig_Filter_Function('array_to_attr'),
+		);
+	}
+
+	public function getTests()
+	{
+		return array(
+			'bool' => new Twig_Test_Function('is_bool')
 		);
 	}
 
@@ -39,4 +53,8 @@ class Twig_Indigo_Extension extends Twig_Extension
 		return urlencode(Uri::create('assets/theme/img/icons/' . ($model->group_id == 6 ? 'admin' : ($model->group_id == 1 ? 'banned' : 'user') ) . '.png'));
 	}
 
+	public function bool($value)
+	{
+		return is_bool($value) ? ($value ? 'true' : 'false') : $value;
+	}
 }
