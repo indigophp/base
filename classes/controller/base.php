3c104b74ed5ff9b1322538f76f7bfcbc36ce44ff
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-01-28 13:09:36 +0100

    Theme path refactored

--- classes/controller/base.php
+++ classes/controller/base.php
@@ -23,16 +23,37 @@ class Controller_Base extends \Controller_Theme
 
 			$theme_name = \Arr::get($this->theme->active(), 'name');
 
+			$use_modules = $this->theme->get_config('use_modules');
+			$path_prefix = null;
+			$module_path = null;
+
+			if ($use_modules and $request = \Request::active() and $module = $request->module)
+			{
+				// we're using module name prefixing
+				$path_prefix = $module.DS;
+
+				// and modules are in a separate path
+				is_string($use_modules) and $path_prefix = trim($use_modules, '\\/').DS.$path_prefix;
+
+				// do we need to check the module too?
+				$use_modules === true and $module_path = \Module::exists($module).'themes'.DS;
+			}
+
 			foreach ($this->theme->get_parent_themes($theme_name) as $theme)
 			{
 				foreach ($this->theme->get_paths() as $path)
 				{
-					if (is_dir($path .= $theme['name']))
-					{
-						$paths[] = $path ;
-					}
+					$paths[] = $path . $theme['name'];
+				}
+
+				if ($use_modules and $module)
+				{
+					$paths[] = $theme['path'] . $path_prefix;
+					$paths[] = $module_path.$theme['name'].DS;
 				}
 			}
+
+			$paths = array_filter($paths, 'is_dir');
 
 			\Config::set('parser.View_Twig.views_paths', $paths);
 		}
