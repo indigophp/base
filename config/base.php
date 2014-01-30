cc741b2d7e83a9f5edad9dd5eb8066f9f127f411
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-01-30 23:34:42 +0100

    Config

---
+++ config/base.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Part of the Indigo framework.
+ *
+ * @package    Indigo
+ * @subpackage Base
+ * @version    1.0
+ * @author     Indigo Development Team
+ * @license    MIT License
+ * @copyright  2013 - 2014 Indigo Development Team
+ * @link       https://indigophp.com
+ */
+
+/**
+ * NOTICE:
+ *
+ * If you need to make modifications to the default configuration, copy
+ * this file to your app/config folder, and make them in there.
+ *
+ * This will allow you to upgrade fuel without losing your custom config.
+ */
+
+
+return array(
+	'theme' => array(
+		'frontend' => 'default',
+		'admin'  => 'default',
+	),
+);
