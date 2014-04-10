bb5b05b40f9b144d2148213122096351709a5d89
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-04-11 00:27:06 +0200

    Added opauth initally

--- modules/auth/classes/controller/auth.php
+++ modules/auth/classes/controller/auth.php
@@ -25,10 +25,10 @@ class Controller_Auth extends \Controller_Base
 	public function action_login()
 	{
 		// Already logged in
-		\Auth::check() and \Response::redirect(\Input::get('uri') ? : '/user');
+		\Auth::check() and $this->redirect();
 
 		$this->template->title = gettext('Login');
-		$this->template->content = $this->theme->view('auth/login', array('val' => ''), false);
+		$this->template->content = $this->theme->view('auth/login');
 	}
 
 	public function post_login()
@@ -56,6 +56,15 @@ class Controller_Auth extends \Controller_Base
 
 			if ($auth->login())
 			{
+				if (\Input::post('remember_me', false))
+				{
+					\Auth::remember_me();
+				}
+				else
+				{
+					\Auth::dont_remember_me();
+				}
+
 				$current_user = \Model\Auth_User::find_by_username(\Auth::get_screen_name());
 				\Session::set_flash('success', \Str::trans(
 					gettext('Welcome, %fullname%!'),
@@ -75,13 +84,90 @@ class Controller_Auth extends \Controller_Base
 			\Session::set_flash('errors', $result->getErrors());
 		}
 
-		$this->template->title = gettext('Login');
-		$this->template->content = $this->theme->view('auth/login', array('val' => $result), false);
+		return \Reponse::redirect();
 	}
 
 	public function action_logout()
 	{
 		\Auth::logout();
 		\Response::redirect('/auth');
+	}
+
+	public function action_oauth($provider = null)
+	{
+		\Auth_Opauth::forge();
+	}
+
+	public function action_callback()
+	{
+		// Opauth can throw all kinds of nasty bits, so be prepared
+		try
+		{
+			// get the Opauth object
+			$opauth = \Auth_Opauth::forge(false);
+
+			// and process the callback
+			$status = $opauth->login_or_register();
+
+			// fetch the provider name from the opauth response so we can display a message
+			$provider = $opauth->get('auth.provider', '?');
+
+			// deal with the result of the callback process
+			switch ($status)
+			{
+				// a local user was logged-in, the provider has been linked to this user
+				case 'linked':
+					// inform the user the link was succesfully made
+					\Messages::success(sprintf(__('login.provider-linked'), ucfirst($provider)));
+					// and set the redirect url for this status
+					$url = 'auth/user';
+				break;
+
+				// the provider was known and linked, the linked account as logged-in
+				case 'logged_in':
+					// inform the user the login using the provider was succesful
+					\Messages::success(sprintf(__('login.logged_in_using_provider'), ucfirst($provider)));
+					// and set the redirect url for this status
+					$url = 'auth/user';
+				break;
+
+				// we don't know this provider login, ask the user to create a local account first
+				case 'register':
+					// inform the user the login using the provider was succesful, but we need a local account to continue
+					\Messages::info(sprintf(__('login.register-first'), ucfirst($provider)));
+					// and set the redirect url for this status
+					$url = 'auth/register';
+				break;
+
+				// we didn't know this provider login, but enough info was returned to auto-register the user
+				case 'registered':
+					// inform the user the login using the provider was succesful, and we created a local account
+					\Messages::success(__('login.auto-registered'));
+					// and set the redirect url for this status
+					$url = 'auth/user';
+				break;
+
+				default:
+					throw new \FuelException('Auth_Opauth::login_or_register() has come up with a result that we dont know how to handle.');
+			}
+
+			// redirect to the url set
+			\Response::redirect($url);
+		}
+
+		// deal with Opauth exceptions
+		catch (\OpauthException $e)
+		{
+			\Messages::error($e->getMessage());
+			\Response::redirect_back();
+		}
+
+		// catch a user cancelling the authentication attempt (some providers allow that)
+		catch (\OpauthCancelException $e)
+		{
+			// you should probably do something a bit more clean here...
+			exit('It looks like you canceled your authorisation.'.\Html::anchor('users/oath/'.$provider, 'Click here').' to try again.');
+		}
+
 	}
 }
