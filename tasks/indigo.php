aea7cc29e685dae6370f5c88d441346ed9bc20c5
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-06-23 20:20:42 +0200

    Adds install task

---
+++ tasks/indigo.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Indigo Base package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fuel\Tasks;
+
+/**
+ * IndigoPHP framework base task
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class Indigo
+{
+
+	/**
+	 * Basic install task
+	 */
+	public function install()
+	{
+
+	}
+}
