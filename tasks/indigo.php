3ab10f71e25f00f0d66ce9aeb840c22667a83a57
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-06-24 00:41:37 +0200

    Adds install task

--- tasks/indigo.php
+++ tasks/indigo.php
@@ -11,6 +11,8 @@
 
 namespace Fuel\Tasks;
 
+use Oil\Refine;
+
 /**
  * IndigoPHP framework base task
  *
@@ -18,12 +20,48 @@ namespace Fuel\Tasks;
  */
 class Indigo
 {
-
 	/**
 	 * Basic install task
 	 */
 	public function install()
 	{
+		if ($dry_run = \Cli::option('dry-run', false))
+		{
+			\Cli::write('INFO: Running in dry run mode.', 'yellow');
+		}
 
+		$db = \Cli::prompt('Would you like to configure database?', array('y','n'));
+
+		if ($db === 'y')
+		{
+			\Config::load('db', true);
+
+			\Cli::write('NOTE: Currently only PDO is supported. The only settings you can provide is: dsn, username, password.', 'red');
+
+			$dsn = \Cli::prompt('Please enter a DSN', \Config::get('db.default.connection.dsn'));
+			$username = \Cli::prompt('Please enter a username', \Config::get('db.default.connection.username'));
+			$password = \Cli::prompt('Please enter a password', \Config::get('db.default.connection.password'));
+
+			if ($dry_run === false)
+			{
+				\Config::set('db.default.connection.dsn', $dsn);
+				\Config::set('db.default.connection.username', $username);
+				\Config::set('db.default.connection.password', $password);
+
+				\Config::save('db', 'db');
+			}
+		}
+		else
+		{
+			\Cli::write('INFO: Database is not configured.', 'yellow');
+		}
+
+		$mgr = \Cli::prompt('Would you like to run migrations now?', array('y','n'));
+
+		if ($mgr === 'y' and $dry_run === false)
+		{
+			\Cli::set_option('all', true);
+			Refine::run('migrate');
+		}
 	}
 }
