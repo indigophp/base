8034111f3e60291ce8ff3105a673cfea50cd3682
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-06-27 20:56:34 +0200

    Refactors theme controller

--- classes/Controller/ThemeController.php
+++ classes/Controller/ThemeController.php
@@ -26,17 +26,18 @@ class ThemeController extends \Controller
 	public $template = 'template';
 
 	/**
-	 * Theme type (eg. frontend or backend)
+	 * Theme type (eg. frontend or admin)
 	 *
 	 * @var string
 	 */
 	public $theme_type = 'frontend';
 
 	/**
-	 * Load the template and create the $this->theme object
+	 * {@inheritdocs}
 	 */
 	public function before($data = null)
 	{
+		// Clones theme instance in case of HMVC request
 		if ($this->request->is_hmvc())
 		{
 			$this->theme = clone \Theme::instance('indigo');
@@ -46,35 +47,47 @@ class ThemeController extends \Controller
 			$this->theme = \Theme::instance('indigo');
 		}
 
-		if ( ! $this->theme->find(\Config::get('indigo.theme.' . $this->theme_type)))
+		// Returns active theme based on theme type
+		$theme = \Config::get('indigo.theme.' . $this->theme_type);
+
+		// Sets the fallback theme if no match found
+		if ($this->theme->find($theme) === false)
 		{
-			\Config::set('indigo.theme.' . $this->theme_type, 'default');
+			$fallback = $this->theme->fallback();
+			$theme = $fallback['name'];
+
+			\Config::set('indigo.theme.' . $this->theme_type, $theme);
 			\Config::save('indigo', 'indigo');
 		}
 
-		$this->theme->active(\Config::get('indigo.theme.' . $this->theme_type, 'default'));
+		$this->theme->active($theme);
 
+		// Sets theme extension
 		if ($engine = $this->theme->get_info('engine'))
 		{
 			$this->theme->set_config('view_ext', '.' . $engine);
 		}
 
+		// Sets template
 		if ( ! empty($this->template) and is_string($this->template))
 		{
 			$this->template = $this->theme->set_template($this->template);
 		}
 
+		// Makes the theme instance available in all views
 		$this->template->set_global('theme', $this->theme, false);
 
 		return parent::before($data);
 	}
 
 	/**
+	 * {@inheritdocs}
+	 *
 	 * keep the after() as standard as possible to allow custom responses from actions
 	 */
 	public function after($response)
 	{
-		// If no response object was returned by the action,
+		// If no response object was returned by the action
 		if (empty($response) or  ! $response instanceof \Response)
 		{
 			// render the defined template
