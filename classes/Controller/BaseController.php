778b4aa80bbe9e8f2b986a86454176df653aae9c
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-07-25 00:51:09 +0200

    Moves theme logic to core

--- classes/Controller/BaseController.php
+++ classes/Controller/BaseController.php
@@ -34,43 +34,7 @@ class BaseController extends \Controller_Theme
 
 		if ('twig' == $this->theme->get_info('engine'))
 		{
-			$paths = array();
-
-			$theme_name = \Arr::get($this->theme->active(), 'name');
-
-			$use_modules = $this->theme->get_config('use_modules');
-			$path_prefix = null;
-			$module_path = null;
-
-			if ($use_modules and $request = \Request::active() and $module = $request->module)
-			{
-				// we're using module name prefixing
-				$path_prefix = $module.DS;
-
-				// and modules are in a separate path
-				is_string($use_modules) and $path_prefix = trim($use_modules, '\\/').DS.$path_prefix;
-
-				// do we need to check the module too?
-				$use_modules === true and $module_path = \Module::exists($module).'themes'.DS;
-			}
-
-			foreach ($this->theme->get_parent_themes($theme_name) as $theme)
-			{
-				if ($use_modules and $module)
-				{
-					$paths[] = $theme['path'] . $path_prefix;
-					$paths[] = $module_path.$theme['name'].DS;
-				}
-
-				foreach ($this->theme->get_paths() as $path)
-				{
-					$paths[] = $path . $theme['name'];
-				}
-			}
-
-			$paths = array_filter($paths, 'is_dir');
-
-			\Config::set('parser.View_Twig.views_paths', $paths);
+			\Config::set('parser.View_Twig.views_paths', $this->theme->get_all_paths());
 		}
 	}
 }
