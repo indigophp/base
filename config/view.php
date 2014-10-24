0a3a3eb36afb54050c689983c2ffdeadb1bf87c5
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-24 10:47:59 +0200

    Adds twig parser

---
+++ config/view.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the IndigoPHP framework.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+	'parsers' => [
+		'twig',
+	],
+];
