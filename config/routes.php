06a12d88ac05b324e66753f5c778880ac7bf5a07
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-24 10:47:47 +0200

    Adds route config

---
+++ config/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the IndigoPHP framework.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * NOTICE:
+ *
+ * This is the route configuration for this FuelPHP application.
+ * It contains configuration which is for this application only.
+ */
+
+// 404 route
+$this->router->all(null, 'welcome/404', '404');
+
+// homepage route
+$this->router->all('/', 'welcome/index', 'root');
