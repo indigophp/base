8121d6fe20178d5f29ca0955f184b832de7ce27c
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-28 01:27:42 +0100

    Adds asset controller

--- config/routes.php
+++ config/routes.php
@@ -21,3 +21,9 @@ $this->router->all(null, 'welcome/404', '404');
 
 // homepage route
 $this->router->all('/', 'welcome/index', 'root');
+
+$this->router->get('themes/{segment}/{any}')
+	->filters([
+		'controller' => 'Indigo\Common\Controller\Assets',
+		'action' => 'index',
+	]);
